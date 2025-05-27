51caf27d07219d8175af75b4c89ef16b8ec6938f
MarcRoviraP <MarcRoviraP@users.noreply.github.com>
2025-05-27 14:42:29 +0000

    deploy: 172f532afceb85761d61d46960187fe22354fafa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"f43bd5c131fd7baae97c31896004d45f85bde6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "119690208"
+    serviceWorkerVersion: "3821092570"
   }
 });
